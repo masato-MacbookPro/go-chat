d6d339a5e20c9f1168cda3065edbfa18cbdf534c
masato-MacbookPro <nakata_masato@cyberagent.co.jp>
2023-03-19 19:51:16 +0900

    sqlboiler生成

--- migrations/20230319164431-create_users.sql
+++ migrations/20230319164431-create_users.sql
@@ -1,5 +1,12 @@
-
 -- +migrate Up
-CREATE TABLE IF NOT EXISTS users (id int not null, name varchar(30) not null);
+CREATE TABLE IF NOT EXISTS users (
+    `id` BIGINT NOT NULL AUTO_INCREMENT, 
+    `name` VARCHAR(30) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4;
+
 -- +migrate Down
 DROP TABLE IF EXISTS users;
